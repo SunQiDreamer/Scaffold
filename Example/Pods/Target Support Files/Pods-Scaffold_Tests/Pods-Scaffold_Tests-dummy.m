9479c33a5fb9aa1af674d843c769be1b366e789b
孙琦 <993473797@qq.com>
2020-12-18 17:35:02 +0800

    Update

---
+++ Example/Pods/Target Support Files/Pods-Scaffold_Tests/Pods-Scaffold_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scaffold_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Scaffold_Tests
+@end
---
+++ Example/Pods/Target Support Files/Scaffold/Scaffold-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Scaffold : NSObject
+@end
+@implementation PodsDummy_Scaffold
+@end
